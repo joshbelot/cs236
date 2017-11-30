c5c138824fe05d1f0826a4010c1fdb32d15e823b
Paul Risk <paulrisk3@gmail.com>
2017-11-30 13:39:39 -0700

    Setup of database files and guide comments

---
+++ rel_database/relation.h
@@ -0,0 +1,4 @@
+// This class needs to implement the three relational algebra methods of
+// select, project, and rename.
+// Queries are made up of these three elements and a table.
+// The database class pulls from here and the table class to perform the queries. 
---
+++ rel_database/table.h
@@ -0,0 +1,3 @@
+// This class needs to get information from the datalog parser and create
+// a table, with a table name, vector<string> headers, and set<vector<string>> rows
+// The database class pulls from here and relations to perform the queries. 
